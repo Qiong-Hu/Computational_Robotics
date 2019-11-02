3da27876abf553e43369123c74b62a6a17e80f40
Maggie Xiao <zxiao2015@g.ucla.edu>
2019-11-02 14:38:25 -0700

    2a

--- lab_2/RRT.py
+++ lab_2/RRT.py
@@ -27,3 +27,51 @@ D_S = 3     # The dimensionality is 3
 # w = 2 * math.pi * R * (w1 - w2) / W rad/s     # positive: clockwise, negative: anti-clockwise
 D_O = 3     # The dimensionality is 3
 
+# Problem 1(b)
+# Define the (continuous space) system model. Assume a fully observable system, i.e. the state itself is directly accessible with no sensor model.
+
+# state space：a m*n rectangular environment with any direction of robot
+m = 1
+n = 1
+
+# obstacles : a list to store obstacle states, each obstacle [xi,yi,wi,hi] is a rectangular, xi,yi represents the lower left corner of the obstacle and wi, hi represents the width and height of the obstacle
+# obstacles= [[xi,yi,wi,hi],...]
+
+# start state: s0=[xs,ys,θs]
+s0=[0, 0, 0]
+    
+# target state: s1=[xt,yt,θt]
+s1=[0, 0, 0]
+
+
+
+# Trajectory Planning
+# 2(a). Given a set of points V in C-space and a single target point xt, dene and justify a metric that can be used to determine which of the points in V is closest to xt.
+
+#RRT Node
+class Node():
+    def __init__(self, state):
+        self.state = state
+        self.parent = None
+
+#determine which of the points in V is closest to xt
+def find_closestNode(V, xt):
+    """ 
+    given a set of RRT nodes <V> in C-space and a single target point <xt>
+    return the closest node to the target point inside the set
+    """
+    Closestnode = V[0]
+    weighted_dist = ((V[0][0] - xt[0])/(math.pi * R * 2))**2 +
+    				((V[0][1] - xt[1])/(math.pi * R * 2))**2 + 
+    				((V[0][2] - xt[2])/(math.pi * R * 4/W))**2
+
+    for point in V:
+        dist = ((point[0] - xt[0])/(math.pi * R * 2))**2 + 
+        	   ((point[1] - xt[1])/(math.pi * R * 2))**2 + 
+        	   ((point[2] - xt[2])/(math.pi * R * 4/W))**2
+        if dist < weighted_dist:
+            Closestnode = point
+            weighted_dist = dist
+    return Closestnode
+
+
