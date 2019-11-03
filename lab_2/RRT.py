c4d96197c35e4f3b919a331bdb926a3be3bebb23
June <wojiaoziyou@163.com>
2019-11-02 18:24:58 -0700

    Update 2b

--- lab_2/RRT.py
+++ lab_2/RRT.py
@@ -8,7 +8,11 @@ import time
 R = 25      # unit: mm, radius of the wheels
 L = 100     # unit: mm, length of the robot
 W = 90      # unit: mm, width of the robot
-wmax = 1    # unit: RPS, maximum angular velocity of the wheels
+
+# Maximum values
+wmax = 1    # unit: RPS (rotations per second), maximum angular velocity of the wheels
+vx_max = vy_max = 2 * math.pi * R * wmax    # unit: mm/s, maxium velocity of the robot in the direction of x and y
+wmax_robot = 4 * math.pi* R * wmax / W      # unit: rad/s, maximum angular velocity of the robot, = max{2 * pi * R * (w1 - w2) / W}
 
 show_animation = True
 
@@ -39,9 +43,10 @@ D_O = 3     # The dimensionality is 3
 # Define the (continuous space) system model
 # Assume a fully observable system, i.e. the state itself is directly accessible with no sensor model.
 
-# state space： a m * n rectangular environment with any direction of the robot
-m = 1
-n = 1
+# Define map
+# a m * n rectangular environment
+m = 10000   # unit: mm, length of the map
+n = 10000   # unit: mm, width of the map
 
 # obstacles: a list to store obstacle states, each obstacle [xi, yi, wi, hi] is a rectangle, (xi, yi) is the coordinate of the lower left-corner of the obstacle, (wi, hi) is the width and height of the obstacle
 # obstacles= {obstacle}, obstacle = [xi, yi, wi, hi]
@@ -67,24 +72,20 @@ class Node():
 # determine which of the points in V is closest to xt
 def find_closestNode(V, xt):
     # V: a set of RRT points. V = {v}, v = (x, y, theta) in C-space
-    # xt: a single target point. xt = (xt1, yt1, thetat1)
+    # xt: a single target point. xt = (x_t, y_t, theta_t)
     # Return: the closest node to the target point xt inside the set V
 
     closest_node = V[0]
 
     # Calculate the weighted distance between a point in set V to the target point xt
-    # For delta_x: distance between x and xt1 divided by the maximum velocity of the robot, vx_max = 2 * pi * R * wmax
-    # For delta_y: distance between y and yt1 divided by the maximum velocity of the robot, vy_max = 2 * pi * R * wmax
-    # For delta_theta: difference between theta and thetat1 divided by the maximum angular velocity of the robot: wmax_robot = max{2 * pi * R * (w1 - w2)/W} = 4 * pi* R * wmax/W
-    # By dividing by the maximum value of each motion, the weighted distance of three different dimensions that the robot can reach are normalized.
-    weighted_dist = ((V[0][0] - xt[0])/(2 * math.pi * R * wmax))**2 +
-                    ((V[0][1] - xt[1])/(2 * math.pi * R * wmax))**2 + 
-                    ((V[0][2] - xt[2])/(4 * math.pi * R * wmax/W))**2
+    # For delta_x: distance between x and x_t divided by the maximum velocity of the robot vx_max
+    # For delta_y: distance between y and y_t divided by the maximum velocity of the robot vy_max
+    # For delta_theta: difference between theta and thetat1 divided by the maximum angular velocity of the robot wmax_robot
+    # By dividing by the maximum value of each motion, the weighted distance of three different dimensions that the robot can reach is normalized.
+    weighted_dist = ((V[0][0] - xt[0])/(2 * math.pi * R * wmax))**2 + ((V[0][1] - xt[1])/(2 * math.pi * R * wmax))**2 + ((V[0][2] - xt[2])/(4 * math.pi * R * wmax / W))**2
 
     for point in V:
-        dist = ((point[0] - xt[0])/(2 * math.pi * R * wmax))**2 +
-               ((point[1] - xt[1])/(2 * math.pi * R * wmax))**2 +
-               ((point[2] - xt[2])/(4 * math.pi * R * wmax/W))**2
+        dist = ((point[0] - xt[0])/(2 * math.pi * R * wmax))**2 + ((point[1] - xt[1])/(2 * math.pi * R * wmax))**2 + ((point[2] - xt[2])/(4 * math.pi * R * wmax / W))**2
         if dist < weighted_dist:
             closest_node = point
             weighted_dist = dist
@@ -95,73 +96,74 @@ def find_closestNode(V, xt):
 # Given arbitrary initial robot state xi and target robot state xt (in C-space), generate a smooth achievable trajectory (that matches the metric you defined above) from the xi towards xt lasting 1 second. What are the control inputs for this trajectory?
 
 def generate_trajectory(xi, xt):
-    """
-    Given initial and 
-    """
-    # trajectory
+    # Given initial and target robot state: xi, xt in C-space.
+    # Return: an achievable trajectory
+
+    # a list to store each point in the trajectory within one second
     trajectory = []
-    # time remain
+
+    # remaining time within one second, updated after each step
     remain_time = 1
-    # end point
-    end0 = [xi[0],xi[1],0]
 
-    # goal-start vector
-    diffx = xt[0]-xi[0]
-    diffy = xt[1]-xi[1]
-    diffd = np.sqrt(diffx**2+ diffy**2)
-    # angle between +x and goal-start vector
-    angle = np.arctan2(diffy,diffx)
-    diffangle = (xi[2] - angle)%math.pi
-    if diffangle<= math.pi/2:
-        end0[2] = xi[2] - diffangle
+    # end point of each step
+    end = [xi[0], xi[1], 0]
+
+    # start-to-goal vector
+    diff_x = xt[0] - xi[0]
+    diff_y = xt[1] - xi[1]
+    diff_dist = math.sqrt(diff_x ** 2 + diff_y ** 2)
+
+    # angle between +x and start-to-goal vector
+    angle = math.atan(diff_y / diff_x)
+
+    # update angle of the end point of each step according to the angle difference
+    diff_angle = (xi[2] - angle) % math.pi
+    if diff_angle <= math.pi / 2:
+        end[2] = xi[2] - diff_angle
     else:
-        end0[2] = xi[2] + (math.pi-diffangle)
-    trajectory.append(end0)
+        end[2] = xi[2] + (math.pi - diff_angle)
+    trajectory.append(end)
     # print(trajectory)
-
-    t0 =  diffangle%(math.pi/2)/(math.pi *R * 4/W)
-    remain_time = 1 - t0
-    needed_time = diffd/(2 * math.pi * R)
-    end1=[xi[0],xi[1],end0[2]]
-    if remain_time<needed_time:
-        dist = remain_time* 2 * math.pi * R
-        end1[0] = xi[0] + dist*np.cos(angle)
-        end1[1] = xi[1] + dist*np.sin(angle)
-        trajectory.append(end1)
+    
+    req_rot_time=  diff_angle % (math.pi / 2) / wmax_robot
+    remain_time = 1 - req_rot_time
+    req_dis_time = diff_dist / (2 * math.pi * R)
+    end = [end[0], end[1], end[2]]
+    if remain_time < req_dis_time :
+        dist = remain_time * 2 * math.pi * R
+        end = [xi[0] + dist * math.cos(angle), xi[1] + dist * math.sin(angle), end[2]]
+        trajectory.append(end)
         # print(trajectory)
-
     else:
-        end1[0] = xt[0]
-        end1[1] = xt[1]
-        trajectory.append(end1)
+        end = [xt[0], xt[1], end[2]]
+        trajectory.append(end)
         # print(trajectory)
-        end2 = [xt[0],xt[1],end0[2]]
-        remain_time -= needed_time
-        diffangle = (end2[2] - xt[2])%(2 * math.pi)
-        needed_time = (diffangle % math.pi) / (math.pi *R * 4/W)
-        if remain_time>=needed_time:
+
+        remain_time -= req_dis_time 
+        diff_angle = (end[2] - xt[2]) % (2 * math.pi)
+        req_dis_time = (diff_angle % math.pi) / wmax_robot
+        if remain_time >= req_dis_time:
             trajectory.append(xt)
             # print(trajectory)
         else:
-            if 0<= diffangle <= math.pi:
-               end2[2] = end2[2] - math.pi *R * 4/W*remain_time
+            if 0<= diff_angle <= math.pi:
+               end[2] = end[2] - wmax_robot * remain_time
             else:
-                end2[2] = end2[2] + math.pi *R * 4/W*remain_time
-            trajectory.append(end2)
+                end[2] = end[2] + wmax_robot * remain_time
+            trajectory.append(end)
             # print(trajectory)
     return trajectory
 
-def plottrajectory(trajectory):
-    plt.figure()
+def plottrajectory(trajectory, ax):
     for point in trajectory:
-        plt.arrow(point[0]+5, point[1]+5, 0.5*np.cos(point[2]),
-          0.5*np.sin(point[2]), color='r', width=10)
-        plt.plot(point[0], point[1],'bo')
-    plt.ylim([-100, 1000])
-    plt.xlim([-100, 1000])
-    plt.grid()
-    plt.show()
+        # plot center point
+        plt.plot(point[0], point[1], 'bo')
+        # plot robot, undo
+    return ax
 
+# For debug
+# fig = plt.figure()
+# ax = fig.add_subplot(1,1,1)
+# plottrajectory([[0,0,0],[0,1,0],[1,1,0]], fig)
+# plt.show()
 
-# Problem 2(c)
-# Given a smooth robot trajectory in C-space and obstacles defined in operational space, determine whether this trajectory is collision-free.
